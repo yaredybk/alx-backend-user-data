a46555c0ad1c64ad490b0f6d68ca923cb77ad63f
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-09-01 02:23:18 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 """0x00. Personal data
 """
+import logging
 import re
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format list of fields."""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
 
 
 def filter_datum(
@@ -20,3 +40,4 @@ def filter_datum(
     """
     pattern = rf"({'|'.join(fields)})\=([^={separator}]*)"
     return re.sub(pattern, rf"\1={redaction}", message)
+
