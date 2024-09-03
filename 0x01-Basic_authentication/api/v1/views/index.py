58f8631a7461954672d8cb2f377a89244d2a598f
yared b. <yb12ybk@gmail.com>
2024-09-03 23:15:26 +0300

    403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> str:
     raise a 401 error by using abort.
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    raise a 403 error by using abort.
+    """
+    abort(403)
