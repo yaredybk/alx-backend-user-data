9c956e9c0c6fc68e2bf4eeeab67ed7582d45098a
yared b. <yb12ybk@gmail.com>
2024-09-12 18:58:55 +0300

    valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Authentication Module """
 import bcrypt
+from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from typing import Union
 from user import User
@@ -40,3 +41,13 @@ class Auth:
 
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
