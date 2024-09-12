b5b86a762942c562d89eb21463216eb9f4d46e33
yared b. <yb12ybk@gmail.com>
2024-09-12 20:48:10 +0300

    password reset

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,29 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """Generate reset password token."""
+    user_email = request.form.get('email', '')
+    is_registered = AUTH.create_session(user_email)
+    if not is_registered:
+        abort(403)
+    token = AUTH.get_reset_password_token(user_email)
+    return jsonify({"email": user_email, "reset_token": token})
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """Update password"""
+    user_email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+    return jsonify({"email": user_email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,25 @@ class Auth:
         except NoResultFound:
             pass
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        updated_token = _generate_uuid()
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=updated_token)
+            return updated_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> str:
+        """ Update password"""
+        if reset_token is None or password is None:
+            return None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)
